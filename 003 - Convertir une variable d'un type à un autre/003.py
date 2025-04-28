df8d8180d772233ef2c4e7e5852734daa5baeb4b
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-28 12:49:57 +0200

    003 - Convertir une variable d'un type à un autre

---
+++ 003 - Convertir une variable d'un type à un autre/003.py
@@ -0,0 +1,15 @@
+# Notions abordées : les variables.
+
+# Dans cet exercice, nous allons afficher un nombre à l'intérieur d'une chaîne de caractères.
+
+# Python ne permet pas de concaténer une chaîne de caractères avec un nombre, il va donc falloir convertir le nombre en chaîne de caractères.
+
+# Votre script devra donc afficher :
+
+# Le nombre est 15 
+
+# nombre = 15
+# print("Le nombre est " + nombre)
+
+nombre = 15
+print("Le nombre est " + str(nombre))
