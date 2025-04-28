614e92af5e5b3e573d68d4ec7842555aa4b57bc4
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-28 12:53:46 +0200

    004 - Trouver la valeur d'une variable

---
+++ 004 - Trouver la valeur d'une variable/004.py
@@ -0,0 +1,18 @@
+# Notions abordées : les variables.
+
+# Pour réussir l'exercice, vous devez printer la valeur que contient la variable a.
+
+# a = 3
+# b = 6
+# a = b
+# b = 7
+
+# print()
+
+
+a = 3
+b = 6
+a = b
+b = 7
+ 
+print(6)
