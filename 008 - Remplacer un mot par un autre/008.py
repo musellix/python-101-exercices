0f9b98b5f7f4538023683238b5c204c1509830cf
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-28 13:27:01 +0200

    008 - Remplacer un mot par un autre

---
+++ 008 - Remplacer un mot par un autre/008.py
@@ -0,0 +1,24 @@
+# 008 - Remplacer un mot par un autre
+# Notions abordées : les chaînes de caractères.
+
+# Dans cet exercice, vous devez remplacer le mot 'Bonjour' dans la variable phrase par le mot 'Bonsoir' à l'aide d'une fonction Python.
+
+# Vous devez donc modifier la variable phrase !
+
+# Votre script devra donc afficher le texte :
+# Bonsoir tout le monde. 
+
+
+# phrase = "Bonjour tout le monde."
+# nouvelle_phrase = 
+# print(nouvelle_phrase)
+
+
+# SOLUTION
+phrase = "Bonjour tout le monde."
+nouvelle_phrase = phrase.replace("Bonjour", "Bonsoir")
+print(nouvelle_phrase)
+
+
+# EXPLICATION
+# Pour remplacer un mot par un autre en Python on utilise la fonction replace.
