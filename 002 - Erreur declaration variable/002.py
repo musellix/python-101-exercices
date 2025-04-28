2610fc459e70069acd06e4280fdca2e8015bb3ab
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-28 12:44:54 +0200

    002 - les variables

---
+++ 002 - Erreur declaration variable/002.py
@@ -0,0 +1,8 @@
+# Notions abordées : les variables.
+
+# Une erreur s'est glissée dans la déclaration de la variable site_web, à vous de la trouver et de la corriger pour que le script affiche le nom du site web.
+# site_web = 'Udemy"
+# print(site_web)
+
+site_web = 'Udemy'
+print(site_web)
