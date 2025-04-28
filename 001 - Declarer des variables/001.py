fe03c0e5dd3c21ad94297cfa16c6ecbe99ee53c5
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-28 12:42:32 +0200

    001 - Declarer des variables

---
+++ 001 - Declarer des variables/001.py
@@ -0,0 +1,17 @@
+# Notions abordées : les variables.
+
+# On commence simple avec ce premier exercice, qui consiste à déclarer différents types de variables.
+
+# La variable 'prenom' qui doit contenir la chaîne de caractère Pierre.
+# La variable 'age' qui doit contenir le nombre 20.
+# La variable 'majeur' qui doit contenir un boolean vrai.
+# La variable 'compte_en_banque' qui doit contenir le nombre décimal 20135,384.
+# La variable 'amis' qui doit contenir une liste contenant trois chaînes de caractères : Marie, Julien et Adrien.
+# La variable 'parents' qui doit contenir un tuple contenant deux chaînes de caractères : Marc et Caroline.
+
+prenom = "Pierre"
+age = 20
+majeur = True
+compte_en_banque = 20135.384 
+amis = ['Marie', 'Julien', 'Adrien']
+parents = ('Marc', 'Caroline')
