04f3e4fbdacdf756c38ac59d9ac14e1dd28b91ec
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-29 13:22:41 +0200

    018 - les listes - recuperer un element sur deux

---
+++ 018 - Les listes - Récupérer un élément sur deux/018.py
@@ -0,0 +1,19 @@
+# 018 - Récupérer un élément sur deux dans une liste
+# Notions abordées : les listes.
+
+# On continue avec les listes. Cette fois-ci, l'objectif de l'exercice est de récupérer un élément sur deux dans la liste.
+
+# Votre script doit donc afficher la liste suivante avec la fonction print :
+# [1, 3, 5, 7, 9] 
+
+# ma_liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# SOLUTION
+ma_liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(ma_liste[::2])
+
+
+# EXPLICATION
+# Pour récupérer un élément sur deux, on utilise le slicing et on indique un pas de 2 à l'intérieur des crochets.
+
