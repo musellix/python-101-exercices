3d58506f897c575b083220358a150f17674b0063
Xavier Muselli <xavier.muselli@sii.fr>
2025-04-29 13:02:29 +0200

    016 - les listes - recuperer un element

---
+++ 016 - Les listes - Récupérer un élément/016.py
@@ -0,0 +1,17 @@
+# 016 - Récupérer un élément dans une liste
+# Notions abordées : les listes.
+
+# On retourne aux bases de Python dans cet exercice dans lequel vous devez tout simplement récupérer le premier élément de la liste.
+
+# Dans cet exemple, vous devez afficher la chaîne de caractères 'Pierre'.
+
+# ma_liste = ["Pierre", "Paul", "Marie"]
+
+
+# SOLUTION
+ma_liste = ["Pierre", "Paul", "Marie"]
+print(ma_liste[0])
+
+
+# EXPLICATION
+# Pour récupérer le premier élément de la liste, on utilise les crochets et l'indice 0.
